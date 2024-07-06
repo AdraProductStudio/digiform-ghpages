45c73ef32fd78399ff04aaebb416297fc17fa36b
AdraProductStudio <adraforyou@gmail.com>
2024-07-06 17:59:54 +0530

    UPDATE : rotation added

--- src/ImageCrop/ImageCropper.jsx
+++ src/ImageCrop/ImageCropper.jsx
@@ -12,6 +12,8 @@ const ImageCropper = ({ image, onCropCancel, onCropDone }) => {
     const [croppedArea, setCroppedArea] = useState(null);
     const [aspectRatio, setAspectRatio] = useState(4 / 3);
 
+    const [rotationDegree, setRotationDegree] = useState()
+
     // Callback when cropping is completed
     const onCropComplete = (croppedAreaPercentage, croppedAreaPixels) => {
         // Store the cropped area in pixels
@@ -21,8 +23,13 @@ const ImageCropper = ({ image, onCropCancel, onCropDone }) => {
     // Callback when the user changes the aspect ratio
     const onAspectRatioChange = (event) => {
         setAspectRatio(event.target.value);
-        console.log(aspectRatio)
+        // console.log(aspectRatio)
     };
+
+    const onDegreeChange = (e) => {
+        setRotationDegree(e.target.value)
+        // console.log(e.target.value)
+    }
 
     return (
         <div className="cropper">
@@ -43,9 +50,10 @@ const ImageCropper = ({ image, onCropCancel, onCropDone }) => {
                             backgroundColor: "#fff",
                         },
                     }}
+                    rotation={rotationDegree}
                 />
-            </div>  
-                <hr />
+            </div>
+            <hr />
             <div className="mb-5 container mx-auto">
                 {/* Aspect ratio selection */}
                 {/* <div className=" mb-4 text-center" onChange={onAspectRatioChange}>
@@ -58,7 +66,13 @@ const ImageCropper = ({ image, onCropCancel, onCropDone }) => {
                     <input className='form-check-input' id='3:1' type="radio" value={3 / 1} name="ratio" /><label className='cursorPointer form-check-label' htmlFor="3:1">  3:1 </label>
                     <input className='form-check-input' id='7:1' type="radio" value={7 / 1} name="ratio" /> <label className='cursorPointer form-check-label' htmlFor="7:1"> 7:1 </label>
                 </div> */}
-
+                <h6>To rotate the image select below : </h6>
+                <div className=" mb-4 col-7 mx-auto" onChange={onDegreeChange}>
+                    <input className='form-check-input' id='0degree' type="radio" value={0} name="degree" /> <label className='cursorPointer form-check-label' htmlFor="0degree"> 0&deg; </label>
+                    <input className='form-check-input' id='90degree' type="radio" value={90} name="degree" /> <label className='cursorPointer form-check-label' htmlFor="90degree"> 90&deg; </label>
+                    <input className='form-check-input' id='180degree' type="radio" value={180} name="degree" /> <label className='cursorPointer form-check-label' htmlFor="180degree"> 180&deg; </label>
+                    <input className='form-check-input' id='360degree' type="radio" value={360} name="degree" /> <label className='cursorPointer form-check-label' htmlFor="360degree"> 360&deg; </label>
+                </div>
                 {/* Buttons for canceling or applying the crop */}
                 <div className="d-flex align-items-center justify-content-between">
                     <button className="btn btn-outline-dark" onClick={onCropCancel}>
